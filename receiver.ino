af4ad2ea4754dcf2151f03f5e890caf70e7a283b
Rakesh Dhakhsina Murthy <rakesh7adm@gmail.com>
2021-03-11 16:24:36 +0000

    Update receiver.ino

--- receiver.ino
+++ receiver.ino
@@ -1,8 +1,8 @@
 /*
 * Arduino Wireless Communication Tutorial
-*       Example 1 - Receiver Code
+*        Receiver Code
 *                
-* by Dejan Nedelkovski, www.HowToMechatronics.com
+* by Rakesh
 * 
 * Library: TMRh20/RF24, https://github.com/tmrh20/RF24/
 */
